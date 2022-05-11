758a49cba4f0a6707e10b9729e53e7710875ea65
thedivtagguy <amanbhargava2001@gmail.com>
2022-05-11 15:06:48 +0530

    Add Pages

--- src/components/Categories.svelte
+++ src/components/Categories.svelte
@@ -7,7 +7,7 @@
         <p class="text-lg font-medium text-center text-gray-500 sm:text-2xl">We've broken up our analysis into three categories, choose one to see tactics that come within it.</p>
         <div class="flex grid h-full grid-cols-12 gap-10 pb-10 mt-8 sm:mt-16">
             <div class="relative flex flex-col items-start justify-end h-full col-span-12 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
-                <a href="/categories/algorithms" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-96 hover:scale-110" style="background-image:url('./static/assets/algorithms.png')">
+                <a href="/categories/algorithms" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-96 hover:scale-110" style="background-image:url('/assets/algorithms.png')">
                 </a>
                 <div class="relative z-20 w-full h-auto py-8 text-white bg-purple-500 border-t-0 border-yellow-200 px-7">
                     <h2 class="mb-5 text-5xl font-bold"><a href="/categories/algorithms">Algorithms</a></h2>
@@ -17,7 +17,7 @@
             </div>
 
             <div class="relative flex flex-col items-start justify-end h-full col-span-12 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
-                <a href="/categories/interfaces" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-96 hover:scale-110" style="background-image:url('./static/assets/interface.png')">
+                <a href="/categories/interfaces" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-96 hover:scale-110" style="background-image:url('/assets/interface.png')">
                 </a>
                 <div class="relative z-20 w-full h-auto py-8 text-white bg-blue-400 border-t-0 border-yellow-200 px-7">
                     <h2 class="mb-5 text-5xl font-bold"><a href="/categories/interfaces">Interfaces</a></h2>
@@ -27,7 +27,7 @@
             </div>
 
             <div class="relative flex flex-col items-start justify-end h-full col-span-12 overflow-hidden rounded-xl group sm:col-span-12 xl:col-span-4 sm:flex-row xl:flex-col">
-                <a href="/categories/community" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-96 hover:scale-110" style="background-image:url('./static/assets/social_context.png')"></a>
+                <a href="/categories/community" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-96 hover:scale-110" style="background-image:url('/assets/social_context.png')"></a>
                 <div class="relative z-20 flex flex-col items-start justify-center w-full h-auto py-8 text-white bg-yellow-400 border-t-0 border-yellow-200 sm:h-full xl:h-auto px-7">
                     <h2 class="mb-5 text-5xl font-bold"><a href="/categories/community">Community</a></h2>
                     <p class="mb-2 text-lg font-normal opacity-100 text-yellow-50">How does Reddit leverage its community for the best experience?
