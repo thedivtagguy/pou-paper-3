47cb50a67745df71b50d669aba2d4e21f8dd12ad
thedivtagguy <amanbhargava2001@gmail.com>
2022-05-12 15:00:54 +0530

    Push Change

---
+++ src/components/Change.svelte
@@ -0,0 +1,90 @@
+
+<!-- Section 1 -->
+<section class="px-2 py-32 bg-white md:px-0">
+    <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+      <div class="flex flex-wrap items-center sm:-mx-3">
+        <div class="w-full md:w-1/2 md:px-3">
+          <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+            <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+              <span class="block xl:inline">Introducing the</span>
+              <span class="block text-red-600 xl:inline">Reddit Paid Partner Program!</span>
+            </h1>
+            <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">It's never been easier to earn more money by posting about things you love!</p>
+            <div class="relative flex flex-col sm:flex-row sm:space-x-4">
+              <a href="#_" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                Try It Free
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+              </a>
+              <a href="#_" class="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2">
+          <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+              <img src="https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGYtbWMxMi1ha2U5NTQ1LWFrZV8xLmpwZw.jpg">
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+<!-- Section 1 -->
+<section class="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
+    <div class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+
+        <!-- Image -->
+        <div class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+            <img src="https://cdn.devdojo.com/images/december2020/productivity.png" class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 ">
+        </div>
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                Boost Your Value
+            </h2>
+            <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+                Get paid to do what you love!
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Price your posts the way you want
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Receive upto 40% profit from every transaction
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Stop creating content for free
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+    </div>
+   
+</section>
+
+  <!-- Section 2 -->
+  <section class="flex items-center justify-center py-10 text-white bg-white sm:py-16 md:py-24 lg:py-32">
+      <div class="relative max-w-3xl px-10 text-center text-white auto lg:px-0">  
+          <!-- Separator -->
+  
+          <!-- Bottom Text -->
+          <h2 class="text-left text-gray-500 xl:text-xl">
+            Reddit is a social media platform that allows users to submit content in the form of posts or links. Other users can then upvote or downvote these submissions, with the most popular submissions appearing on the front page of the site. Recently, Reddit has been experimenting with a new feature that allows users to pay for their submissions to be featured on the front page.
+
+            This is a bad idea for a number of reasons. Firstly, it means that only those with enough money to pay will be able to get their submissions featured. This will create a two-tier system on Reddit, with those who can pay having their submissions seen by more people, and those who can't pay being pushed to the sidelines.
+            
+            Secondly, it's not clear how much money Reddit will be making from this feature. If they're making a lot of money, then it's only a matter of time before they start charging users for other features, such as messaging or viewing certain subreddits. This would be hugely detrimental to the user experience, and would likely lead to mass exodus from the site.
+            
+            Thirdly, and most importantly, this feature goes against the very principle of Reddit, which is that all users are equal. By allowing users to pay for their submissions to be featured, Reddit is giving an advantage to those who can afford it, and that is not fair.
+            
+            In conclusion, the new feature that allows users to pay for their submissions to be featured on the front page of Reddit is a bad idea. It will create a two-tier system, it's not clear how much money Reddit will be making from it, and it goes against the principle of equality that is at the heart of the site.
+            
+            
+            
+                      </h2>
+      </div>
+  </section>
+  
